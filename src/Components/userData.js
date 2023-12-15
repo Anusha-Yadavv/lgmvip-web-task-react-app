68a19c8c5ba2ee157297cfe78a64b85c3fe51e0f
Anusha-Yadavv <anushakottam22@gmail.com>
2023-12-15 14:21:11 +0530

    Showing a loader while the API fetches the data.

--- src/Components/userData.js
+++ src/Components/userData.js
@@ -2,31 +2,49 @@ import React from "react";
 import { useState } from "react";
 import UserDetails from "./UserDetails";
 import "./userData.css";
+import "./Loader.css";
 
 const UserData = () => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const loadUsers = async () => {
-    const response = await fetch("https://reqres.in/api/users?page=1");
-    const jsonResponse = await response.json();
-    console.log("JSON RESPONSE", jsonResponse.data);
-    setUsers(jsonResponse.data);
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://reqres.in/api/users?page=1");
+      const jsonResponse = await response.json();
+      setUsers(jsonResponse.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   console.log("users", users);
   console.log("users data", users.data);
   return (
     <div className="main-container">
       <h1 className="heading">Hello All</h1>
-      <button className="glow-on-hover" onClick={loadUsers}>
-        Get Users
+      <button
+        className="glow-on-hover"
+        onClick={loadUsers}
+        disabled={isLoading}
+      >
+        {isLoading ? "Loading..." : "Get Users"}
       </button>
       <h2>Users:</h2>
-      {/* <UserDetails/> */}
 
       <section>
-        {console.log("Im printing")}
-        {Array.from(users).map((obj) => (
-          <UserDetails key={Math.random()} details={obj} />
-        ))}
+        {isLoading ? (
+          <p>
+            <span class="loader"></span>
+          </p>
+        ) : (
+          Array.from(users).map((obj) => (
+            <UserDetails key={Math.random()} details={obj} />
+          ))
+        )}
       </section>
     </div>
   );
